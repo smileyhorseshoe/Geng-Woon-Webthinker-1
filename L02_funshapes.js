5213662f9c7a5b83157e1ee8389b3eac6bdc3587
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-07-16 08:26:54 +0000

    Jul 16, 2025, 8:26 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -7,7 +7,7 @@ function setup() {
 }
 function draw() {
   // Challenge 1: An eye using ellipse() function
-circle(400,60,100)
+circle(400,200,100)
 }
   // Challenge 2: A face using circle() function
 
