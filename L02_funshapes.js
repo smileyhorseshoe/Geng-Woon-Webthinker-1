613338c275fe8a6df7068d38582c49e30fc193b0
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-07-16 08:33:56 +0000

    Jul 16, 2025, 8:33 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -10,7 +10,7 @@ function draw() {
 rect(100,50,400,400)
 circle(395,200,100)
 circle(200,200,100)
-rect(270,200,100,200)
+rect(250,200,100,200)
 }
   // Challenge 2: A face using circle() function
 
