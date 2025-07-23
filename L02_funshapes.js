59efa184e95ef13b190d401b04552bbd75c38629
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-07-23 08:59:32 +0000

    L03

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -12,16 +12,16 @@ function setup() {
 //  circle(170,200,100)
 //  rect(250,200,100,200)
 //  ellipse(330,300,30,25)
-// }
-function draw() {
-  fill("red")
-  // triangle(200,200,400,200,300,0)
-  strokeWeight(100);
-  stroke(78,173,141)
-  noFill();
-   triangle(200,200,400,200,300,0)
+// // }
+// function draw() {
+//   fill("red")
+//   // triangle(200,200,400,200,300,0)
+//   strokeWeight(100);
+//   stroke(78,173,141)
+//   noFill();
+//    triangle(200,200,400,200,300,0)
  
-  }
+//   }
   // Challenge 2: A face using circle() function
 
   // Challenge 3: A house using rect() function
@@ -33,3 +33,10 @@ function draw() {
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
+function draw() {
+fill("yellow")
+circle(300,200,300)
+noStroke()
+fill("black")
+circle(200,200,50)
+}
--- L03_patterns.js
+++ L03_patterns.js
@@ -1,17 +1,42 @@
+let colorValue = 0;
+posX = 50;
+// g = 235
+// b = 124
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(1200, 600);
     background(220);
-    noLoop(); // Stops continuous drawing
+  //  noLoop(); // Stops continuous drawing
   }
   
 function draw() {
-  // Recap 1: Repeating Circles
+//   let diameter = 80;
+//   let x = 75;
+//   // Recap 1: Repeating Circles
+// for (let i=1; i<10; i++) {
+//   // fill(i * 50)
+//   fill(i*50,235,135)
+//  circle(x,200,diameter);
+//  x = x + diameter + 10;
+
+
+
+
 
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
-
-  // Task 3: Row of Circles
-
+//  fill(colorValue%255,g%255,b%255);
+//  circle(250,250,200);
+//  colorValue++;
+// g+g+2;
+// b+b+2;
+  // Task 3: Colour Alternate
+fill(  (colorValue%2) *255);
+circle(posX,250,100)
+colorValue++;
+posX=posX+105
   // Task 4: Grid of Circles
+// C1
+
+
 }
