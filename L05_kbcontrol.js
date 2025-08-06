15bcd1f1bcaf49cf6dc0295d9b67e99dca496a74
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-06 08:46:17 +0000

    Aug 6, 2025, 8:46 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -11,14 +11,14 @@ background(200)
 // rect(width / 2 - rectsize / 2,height / 2 - rectsize / 2,rectsize, rectsize );
 if (key === 's') {
     rect(50,50,100,100)
-    fill( random(0, 255),random(0, 255),random(0, 255));
+    
 }
 if (key === 'c') {
     circle(width /2,height /2,150)
     fill( random(0, 255),random(0, 255),random(0, 255));
 }
 if (key === 't') {
-    triangle(50,150,150,50,150,150)
+    triangle(0,200,100,0,200,200)
     fill( random(0, 255),random(0, 255),random(0, 255));
 }
 }
