ca40916ac3f26f5fc0f7d0021fdb61a5ad5f3268
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-06 08:34:51 +0000

    Aug 6, 2025, 8:34 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -9,7 +9,7 @@ function setup() {
 function draw() {
 // background(220)
 // rect(width / 2 - rectsize / 2,height / 2 - rectsize / 2,rectsize, rectsize );
-
+if (key === 's')
 }
 // function keyPressed() {
 //     rectsize = 100;
