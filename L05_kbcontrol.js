fa122bfcbdcd917768d55ef4e15f25cf948e03ff
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-06 08:34:26 +0000

    Aug 6, 2025, 8:34 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,23 +1,23 @@
 let diameter = 2.5;
 let rectsize = 50;
 function setup() {
-    createCanvas(600,400);
-    // background(200);
+    createCanvas(200,200);
+    background(200);
     // noLoop();
 }
 
 function draw() {
-background(220)
-rect(width / 2 - rectsize / 2,height / 2 - rectsize / 2,rectsize, rectsize );
+// background(220)
+// rect(width / 2 - rectsize / 2,height / 2 - rectsize / 2,rectsize, rectsize );
 
 }
-function keyPressed() {
-    rectsize = 100;
+// function keyPressed() {
+//     rectsize = 100;
 
-}
-function keyReleased() {
-    rectsize = 50;
-}
+// }
+// function keyReleased() {
+    // rectsize = 50;
+// }
 // function mouseDragged() {
 //     circle(mouseX, mouseY, diameter);
 //     diameter = diameter + 0.5;
