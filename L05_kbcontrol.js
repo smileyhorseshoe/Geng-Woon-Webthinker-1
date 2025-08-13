8b2a66755e0c2bb261eded2d6875cab1c190ee21
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-13 08:33:04 +0000

    Aug 13, 2025, 8:33 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -47,8 +47,8 @@ if (keyIsDown(UP_ARROW)){
 if (keyIsDown(DOWN_ARROW)) {
     ypos = ypos +3;
 }
-ypos = constrain(ypos,0,height = height - diameter)
-// xpos = constrain(xpos,0,width = width)
+// ypos = constrain(ypos,0,height = height - diameter)
+xpos = constrain(xpos,0,width = width - 50)
 fill( random(0, 255),random(0, 255),random(0, 255));
 circle(xpos,ypos,diameter);
 
