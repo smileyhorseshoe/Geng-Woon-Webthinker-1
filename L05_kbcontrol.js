177077625043f1fba4cfe5e4b9b33fdba87ac005
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-13 07:41:24 +0000

    Aug 13, 2025, 7:41 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -22,7 +22,7 @@ background(400,600)
 //     fill( random(0, 255),random(0, 255),random(0, 255));
 // }
 textSize(48);
-text(key ,)
+text(key,    250,70 )
 }
 // function keyPressed() {
 //     rectsize = 100;
