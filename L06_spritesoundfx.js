710140ad9c1580393895f377d76faffab1db2eb4
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-13 08:58:38 +0000

    Aug 13, 2025, 8:58 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,7 +1,7 @@
 let soundEffect,bgMusic,staticImage;
 function preload() {
 soundEffect = loadSound('assets/pop.mp3')
-bgmusic = loadSound('assets')
+bgmusic = loadSound('assets/bossaNova.mp3')
 }
 function setup() {
     createCanvas(1200,600);
