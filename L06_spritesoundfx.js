26f0652fc30f2359161bb6cd480ef83d68d88bf7
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-08-20 08:13:49 +0000

    Aug 20, 2025, 8:13 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -22,7 +22,7 @@ if (keyIsDown(LEFT_ARROW)) {
     xpos = xpos -3;
 }
 if (keyisDown(RIGHT_ARROW)) {
-    xpos = xpos -3;
+    xpos = xpos +3;
 }
 // xpos = constrain(xpos,30,340)
 if (keyIsDown(UP_ARROW)){
