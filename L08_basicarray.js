1667dcb19e656d4310d201c5f7632ccf2098bfc2
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-09-10 08:26:22 +0000

    Sep 10, 2025, 8:26 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -18,7 +18,7 @@ function setup() {
     for ( let index=0; index < favFoods.length; index++) {
     let yValue = 80;
     for (let index =0; index < favFoods.length; index++) {
-        console.log
+        console.log( "I love " + )
     }
     }
 }
