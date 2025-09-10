c9d7c99090484c31f661290f9b1b50f6ed042fb3
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-09-10 08:11:10 +0000

    Sep 10, 2025, 8:11 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,7 +1,7 @@
 let velocityX = 2;
 let xpos = 200;
 let ypos =200;
-
+let favouriteFodos =  []
 
 
 
@@ -10,9 +10,9 @@ function setup() {
     background("black");
     fill("cyan")
     textSize(32);
-    text("My name is Geng Woon", 50,50)
-    text("I am 10 this year", 50, 100)
-    text("My favourite activity is swimming", 50, 150)
+    // text("My name is Geng Woon", 50,50)
+    // text("I am 10 this year", 50, 100)
+    // text("My favourite activity is swimming", 50, 150)
 }
 
 function draw() {
