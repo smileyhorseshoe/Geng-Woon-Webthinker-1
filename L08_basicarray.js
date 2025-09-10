6892337bb06bcf74a67e3d4f79d046d1a794addf
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-09-10 07:52:36 +0000

    Sep 10, 2025, 7:52 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -15,7 +15,7 @@ function setup() {
 
 function draw() {
 rect(300,200,50,50)
-    if (X > widthSize/2) {
+    if (X > width-Size/2) {
         ballSpeedX = ballSpeedX *-1
         fill(random(0,255), random(0,255), random(0,255));
     }
