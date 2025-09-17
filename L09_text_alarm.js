732e1bb52f404d14eac6581c21c51dccacce1128
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-09-17 08:50:21 +0000

    Sep 17, 2025, 8:50 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,21 +1,21 @@
-let xpos;
-let velocityX;
-function setup() {
-    createCanvas(600,400);
-    background(0);
-    fill("cyan")
-    textSize(28)
-    textAlign(CENTER,CENTER)
-    xpos = width/2
-    velocityX = 200;
-}
+// let xpos;
+// let velocityX;
+// function setup() {
+//     createCanvas(600,400);
+//     background(0);
+//     fill("cyan")
+//     textSize(28)
+//     textAlign(CENTER,CENTER)
+//     xpos = width/2
+//     velocityX = 200;
+// }
 
-function draw() {
-    background("black")
-        translate(xpos,height/2)
-    text("Bounce!", 0,0)
-    xpos = xpos + velocityX
-    if(xpos > width||xpos < 0) {
-        velocityX *= -1
-    }
-}
+// function draw() {
+//     background("black")
+//         translate(xpos,height/2)
+//     text("Bounce!", 0,0)
+//     xpos = xpos + velocityX
+//     if(xpos > width||xpos < 0) {
+//         velocityX *= -1
+//     }
+// }
