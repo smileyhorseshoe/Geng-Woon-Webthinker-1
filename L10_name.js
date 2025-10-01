d671b16ff29f9c02ada40bcf3e204c59ad391f93
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-10-01 08:26:21 +0000

    Oct 1, 2025, 8:26 AM

--- L10_name.js
+++ L10_name.js
@@ -7,7 +7,7 @@ function setup() {
 colorPicker = createColorPicker();
 colorPicker.position(width/2-25,height/2)
 userInput = createInput();
-userInput.position(width/2, height/2+100)
+userInput.position(width/2-25, height/2+100)
 }
 
 function draw() {
