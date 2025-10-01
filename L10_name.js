7972d0dec1b6341bdb146c4aa2efb05eeeeeb5a1
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-10-01 08:40:57 +0000

    Oct 1, 2025, 8:40 AM

--- L10_name.js
+++ L10_name.js
@@ -5,7 +5,7 @@ function setup() {
     createCanvas(600,600);
     background("skyblue");
 colorPicker = createColorPicker("skyblue");
-colorPicker.position(width/2-75,height/2)
+colorPicker.position(width/2+20,height/2)
 userInput = createInput();
 userInput.position(width/2, height/2+100)
 userInput.input(updateLabel); 
