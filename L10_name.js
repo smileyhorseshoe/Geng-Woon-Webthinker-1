a0529cda8db9cabf18e0a2404d4f48cc6e95b348
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-10-01 08:35:10 +0000

    Oct 1, 2025, 8:35 AM

--- L10_name.js
+++ L10_name.js
@@ -22,5 +22,5 @@ function draw() {
     textSize(32)
     textAlign(CENTER,CENTER)
     text(username, width/2,150)
-    text("Enter text here:" , width/2-110, height /2+105)
+    text("Enter text here:" , width/2-115, height /2+105)
 }
