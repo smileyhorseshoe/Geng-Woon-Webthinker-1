43f203a15a7e2c029e8ccdbd377e2462320762cd
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-10-08 08:23:22 +0000

    Oct 8, 2025, 8:23 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -13,7 +13,7 @@ function setup() {
     inputSomeText.position(50,150);
     inputSomeText.size(300);
     button1 = createButton("Click on Me");
-    button1.position(50,100);
+    button1.position(50,300);
     button1.size(100,50);
     button1.mousePressed(updateStory);
 }
