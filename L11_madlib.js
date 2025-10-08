621bad585d89f1b86252bf803c79b2de1a1a06ce
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-10-08 07:55:34 +0000

    Oct 8, 2025, 7:55 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -9,6 +9,9 @@ let story;
 function setup() {
     createCanvas(600,400);
     background("skyblue");
+    userInput = createInput(username,"text");
+    userInput.position(width/2, height/2+100);
+    userInput.input(updateLabel); 
 }
 function draw() {
 
