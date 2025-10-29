d66a660430104c673b0209da8b91e175c5d88022
smileyhorseshoe <193582405+smileyhorseshoe@users.noreply.github.com>
2025-10-29 08:42:23 +0000

    Oct 29, 2025, 8:42 AM

--- L15_geometrydash.js
+++ L15_geometrydash.js
@@ -5,7 +5,8 @@ function setup() {
 function draw() {
     rect(0,690,1000,10)
     fill("red")
-    drawSpike(850,880,675,620)
+    // drawSpike(850,880,675,620)
+    triangle()
 }
 // function drawSpike(x1,x2,y1,y2) {
     // fill("blue")
